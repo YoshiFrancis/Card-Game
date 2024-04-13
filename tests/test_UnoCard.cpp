124e61b7c8450ebf4a705f39ea47d03014169261
Francis Ozua <fozua@Franciss-MacBook-Air.local>
2024-04-13 17:57:11 -0400

    CMake testing works
    Its go time

--- tests/test_UnoCard.cpp
+++ tests/test_UnoCard.cpp
@@ -1,10 +1,13 @@
-#include "UnoCard.h"
+#include <catch2/catch_test_macros.hpp>
 
-int main(int argc, char* argv[]) {
-
-
-    UnoCard card;
-    
-    return 1;
+TEST_CASE( "Runs", "[UnoCard]" ) {
+    SECTION ("Checking if it succeeds") {
+        CHECK(1 + 1 == 2);
+    }
 }
 
+TEST_CASE( "Fails", "[UNOCARD]" ) {
+    SECTION("CHECKING IF IT FAILS") {
+        CHECK( 1 + 1 == 3);
+    }
+}
---
+++ tests/test_testing.cpp
@@ -0,0 +1,5 @@
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE () {
+    REQUIRE(1 + 1 == 2);
+}
